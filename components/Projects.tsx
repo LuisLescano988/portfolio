2199e48b2893e3f40ae0565c963f334be473f97f
Luis Lescano <88040429+LuisLescano988@users.noreply.github.com>
2023-07-27 13:38:10 -0300

    update map

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -24,7 +24,7 @@ const projects = [
     {
         name: 'Mini Geolocation',
         description: 'Simple mini map with functions to create and delete projects',
-        image: '/fittnet.jpeg',
+        image: '/geomaplocation.jpeg',
         github: 'https://github.com/LuisLescano988/real-state-geolocation',
         link: 'https://vercel.com/luislescano988/real-state-geolocation'
     },
